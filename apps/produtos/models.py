60e9dd222b660316d2b24f126ae2ad9351fd6103
Mauricio Almeron <malmeron@hotmail.com>
2022-01-10 19:40:00 -0300

    Criando a app Produtos

---
+++ apps/produtos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100,help_text='Nome do Produto')
+
+    def __str__(self):
+        return self.nome
