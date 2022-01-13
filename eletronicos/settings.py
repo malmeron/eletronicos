a5c81575a920505e0128c5e4d3ef048d1839efe8
Mauricio Almeron <malmeron@hotmail.com>
2022-01-13 16:43:27 -0300

    Adicionando o static root corrigido11

--- eletronicos/settings.py
+++ eletronicos/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 #STATIC_URL = 'static/'
-STATIC_URL = '/home/ubuntu/eletronicos/static'
+#STATIC_URL = '/home/ubuntu/eletronicos/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -132,7 +132,15 @@ STATICFILES_DIRS = [
 ]
 #STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 #STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-STATIC_ROOT = '/home/ubuntu/eletronicos/static'
+#STATIC_ROOT = '/home/ubuntu/eletronicos/static'
 
 #fonte: https://github.com/OpenToAllCTF/OTA-University/issues/9
 #STATIC_ROOT = '/static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
