416b4dae8b902408bcd3211e0269a487daeb9abe
Mauricio Almeron <malmeron@hotmail.com>
2022-01-13 16:55:42 -0300

    Adicionando o static root corrigido14

--- eletronicos/settings.py
+++ eletronicos/settings.py
@@ -130,4 +130,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = "/home/ubuntu/eletronicos/static"
