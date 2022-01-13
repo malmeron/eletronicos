f57b917b182b03c4ddb1280eb5ed5f81d8528c19
Mauricio Almeron <malmeron@hotmail.com>
2022-01-13 16:40:34 -0300

    Adicionando o static root corrigido10

--- eletronicos/settings.py
+++ eletronicos/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     '/home/ubuntu/eletronicos/static',
 ]
 #STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+#STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 STATIC_ROOT = '/home/ubuntu/eletronicos/static'
 
 #fonte: https://github.com/OpenToAllCTF/OTA-University/issues/9
