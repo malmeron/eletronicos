bc91f9fc3450d958157b57b196edb97ee01c2f04
Mauricio Almeron <malmeron@hotmail.com>
2022-01-29 19:12:20 -0300

    Colocando endereco do fornecedor como opcional

--- apps/fornecedores/models.py
+++ apps/fornecedores/models.py
@@ -5,6 +5,6 @@ from apps.enderecos.models import Endereco
 # Create your models here.
 class Fornecedor(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do Fornecedor')
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, blank=True)
     def __str__(self):
         return self.nome
